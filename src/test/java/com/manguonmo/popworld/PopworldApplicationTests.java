124286874c405321aeadac76a00309a2b0c8fc8b
luongduyenhop <hopluong309@gmail.com>
2026-09-09 23:39:49 +0700

    chore: initialize PopWorld Spring Boot 3 project with MySQL Docker and dependencies

---
+++ src/test/java/com/manguonmo/popworld/PopworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manguonmo.popworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
